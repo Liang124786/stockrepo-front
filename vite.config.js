cfb9e19eb94176cec7cb099deed66f3c22bb5b6d
Yu-Shiun <n3ph006412@gmail.com>
2026-02-10 14:48:42 +0800

    新增  base: '/stockrepo-front/',

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/stockrepo-front/',
   plugins: [
     vue(),
     vueDevTools(),
