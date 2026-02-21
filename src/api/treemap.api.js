5c67534cf05f30caff2c8cee9ec766b1c12a1d5d
Yu-Shiun <n3ph006412@gmail.com>
2026-02-21 21:45:33 +0800

    增加可讀性

--- src/api/treemap.api.js
+++ src/api/treemap.api.js
@@ -2,5 +2,5 @@ import { api } from './axios'
 
 // 後端固定用 0050，不需要 symbols
 export const getTreemapData = (market) => {
-  return api.post(`/treemap/${market}`).then((r) => r.data.result)
+  return api.post(`/treemap/${market}`).then((response) => response.data.result)
 }
